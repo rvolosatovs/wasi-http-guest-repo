1a0ffe078b577265176c3d472240fcb4fa18ffe0
Roman Volosatovs <rvolosatovs@riseup.net>
2023-03-30 13:34:33 +0200

    feat: implement HTTP guest
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,1 +1,11 @@
+wit_bindgen::generate!("guest");
+
 struct Guest;
+
+impl http::Http for Guest {
+    fn handle(_req: types::IncomingRequest, _res: types::ResponseOutparam) {
+        todo!()
+    }
+}
+
+export_guest!(Guest);
